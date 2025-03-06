2f747da727c52267de505b7a0e93420569eda1c3
Valentyn Sobol <napster_1997@mail.ru>
2025-03-06 19:18:40 +0300

    Upgrade analyzer version to 2025-03-06-1

--- analyze/entrypoint.sh
+++ analyze/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+VERSION=2025-03-06-1
+
 PROJECT_ROOT=$(echo "$ANALYZE_PROJECT_ROOT" | cut -c7- | rev | cut -c7- | rev)
-export PROJECT_ROOT
+echo "Run analyzer on $PROJECT_ROOT"
 
-echo $PROJECT_ROOT
+ANALYZER_ARGS="--build autobuild --ifds-analysis-timeout=1000 --verbosity=info --ifds-ap-mode=Tree"
 
-docker run -i -a stdout -a stderr -v /var/run/docker.sock:/var/run/docker.sock -v "$PROJECT_ROOT":/data/projects/project -v "$PROJECT_ROOT":/data/reports -e CONTAINER_UID="$(id -u)" -e CONTAINER_GID="$(id -g)" --entrypoint='/bin/bash' ghcr.io/explyt/usvm-project-analyzer:2024-11-06-2 -c "echo start; ls -la /data/projects/project; /home/usvm/usvm_docker_entrypoint.sh --build autobuild --projects-root-dir /data/projects/project --output-dir /data/reports --ifds-analysis-timeout=1000 --verbosity=info --ifds-ap-mode=Tree 2>&1; echo finish"
+if [ "$ANALYZE_PROJECT_KIND" = "spring" ]; then
+  ANALYZER_ARGS="$ANALYZER_ARGS --project-kind spring-web";
+fi
+
+if [[ -n $ANALYZE_CONFIG ]]; then
+  ANALYZER_ARGS="$ANALYZER_ARGS --config $ANALYZE_CONFIG";
+fi
+
+echo "Analyzer params: $ANALYZER_ARGS"
+
+docker run -i -a stdout -a stderr -v /var/run/docker.sock:/var/run/docker.sock -v "$PROJECT_ROOT":/data/projects/project -v "$PROJECT_ROOT":/data/reports -e CONTAINER_UID="$(id -u)" -e CONTAINER_GID="$(id -g)" --entrypoint='/bin/bash' explyt/usvm-project-analyzer:"$VERSION" -c "echo start; ls -la /data/projects/project; /home/usvm/usvm_docker_entrypoint.sh --projects-root-dir /data/projects/project --output-dir /data/reports $ANALYZER_ARGS 2>&1; echo finish"
